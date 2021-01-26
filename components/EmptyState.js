201b81716098baf3d79555f33068b578ea64d928
afrie <itsafrie@gmail.com>
2021-01-26 22:13:59 +0800

    Add EmptyState component

---
+++ components/EmptyState.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Heading, Text, Flex } from '@chakra-ui/react'
+import DashboardShell from './DashboardShell'
+import AddSiteModal from './AddSiteModal'
+
+const EmptyState = () => {
+  return (
+    <DashboardShell>
+      <Flex
+        width='100%'
+        backgroundColor='white'
+        borderRadius='8px'
+        p={16}
+        justify='center'
+        direction='column'
+        align='center'
+      >
+        <Heading as='h2' size='lg' mb={2}>
+          You haven't add any sites.
+        </Heading>
+        <Text mb={4}>Welcome 👋🏻 Let's get started</Text>
+        <AddSiteModal />
+      </Flex>
+    </DashboardShell>
+  )
+}
+
+export default EmptyState
