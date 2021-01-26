9990bde2c8ebc5131c1d39a690dfe920df893aa4
afrie <itsafrie@gmail.com>
2021-01-26 18:58:46 +0800

    Modify home page with chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,23 @@
-import Head from 'next/head'
-import { useAuth } from '../lib/auth'
-import styles from '../styles/Home.module.css'
+import { useAuth } from '@/lib/auth'
+import { Button, Code, Heading, Text } from '@chakra-ui/react'
 
 export default function Home() {
   const auth = useAuth()
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <div>
+      <main>
+        <Heading>Fast Feedback</Heading>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href='https://nextjs.org'>Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Text>
+          Current user: <Code>{auth.user ? auth.user.email : 'none'}</Code>
+        </Text>
 
         {auth?.user ? (
-          <button onClick={() => auth.signOut()}>Sign Out</button>
+          <Button onClick={() => auth.signOut()}>Sign Out</Button>
         ) : (
-          <button onClick={() => auth.signInWithGithub()}>Sign In</button>
+          <Button onClick={() => auth.signInWithGithub()}>Sign In</Button>
         )}
-
-        <div>{auth?.user?.email}</div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
