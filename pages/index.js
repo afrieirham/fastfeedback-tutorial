a1b45f8501c70594f03b0e59b5538edc2ce5bebe
afrie <itsafrie@gmail.com>
2021-01-26 22:14:34 +0800

    Redesign landing page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
 import { useAuth } from '@/lib/auth'
-import { Button, Code, Heading, Text } from '@chakra-ui/react'
+import { LogoIcon } from '@/styles/icons/logo'
+import { Button, Flex } from '@chakra-ui/react'
 
 export default function Home() {
   const auth = useAuth()
   return (
     <div>
-      <main>
-        <Heading>Fast Feedback</Heading>
-
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'none'}</Code>
-        </Text>
+      <head>
+        <title>Fast Feedback</title>
+      </head>
+      <Flex as='main' direction='column' align='center' justify='center' h='100vh'>
+        <LogoIcon boxSize='64px' />
 
         {auth?.user ? (
-          <Button onClick={() => auth.signOut()}>Sign Out</Button>
+          <Button mt={4} size='sm' onClick={() => auth.signOut()}>
+            Sign Out
+          </Button>
         ) : (
-          <Button onClick={() => auth.signInWithGithub()}>Sign In</Button>
+          <Button mt={4} size='sm' onClick={() => auth.signInWithGithub()}>
+            Sign In
+          </Button>
         )}
-      </main>
+      </Flex>
     </div>
   )
 }
