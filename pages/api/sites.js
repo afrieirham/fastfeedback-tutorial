95bb8ba7756a7d4314ccc6a6c01c1e0a076ac4fb
afrie <itsafrie@gmail.com>
2021-01-30 02:37:35 +0800

    Refactor /api/sites to use firebase functions

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,15 +1,9 @@
-import db from '@/lib/firebase-admin'
+import { getAllSites } from '@/lib/db-admin'
 
 export default async (_, res) => {
-  const snapshot = await db.collection('sites').get()
-
-  const sites = []
-  snapshot.forEach((doc) => {
-    sites.push({ id: doc.id, ...doc.data() })
-  })
-
-  // Sort sites based on createdAt
-  const sortedSites = sites.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-
-  return res.status(200).json(sortedSites)
+  const { sites, error } = await getAllSites()
+  if (error) {
+    return res.status(500).json({ error })
+  }
+  return res.status(200).json({ sites })
 }
