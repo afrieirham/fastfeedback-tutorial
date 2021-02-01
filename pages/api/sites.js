9f1a9855cd16ffec5f6e49cf9c3a3e274d0d6a95
afrie <itsafrie@gmail.com>
2021-02-01 10:39:19 +0800

    Add user verification in GET /api/sites endpoint

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,9 +1,12 @@
-import { getAllSites } from '@/lib/db-admin'
+import { getUserSites } from '@/lib/db-admin'
+import { auth } from '@/lib/firebase-admin'
 
-export default async (_, res) => {
-  const { sites, error } = await getAllSites()
-  if (error) {
+export default async (req, res) => {
+  try {
+    const { uid } = await auth.verifyIdToken(req.headers.token)
+    const sites = await getUserSites(uid)
+    return res.status(200).json(sites)
+  } catch (error) {
     return res.status(500).json({ error })
   }
-  return res.status(200).json({ sites })
 }
