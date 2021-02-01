b5146c5de5239da963f4695c5a153865d07dc945
afrie <itsafrie@gmail.com>
2021-02-01 18:00:34 +0800

    Create /api/feedback endpoint

---
+++ pages/api/feedback.js
@@ -0,0 +1,12 @@
+import { getUserFeedback } from '@/lib/db-admin'
+import { auth } from '@/lib/firebase-admin'
+
+export default async (req, res) => {
+  try {
+    const { uid } = await auth.verifyIdToken(req.headers.token)
+    const feedback = await getUserFeedback(uid)
+    return res.status(200).json(feedback)
+  } catch (error) {
+    return res.status(500).json({ error })
+  }
+}
